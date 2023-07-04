204a06252ea0b14778ceedb5419992ff3baa31fd
Octavy <octavyrun@gmail.com>
2023-07-04 23:48:17 +0300

    firebase integration

---
+++ landing-page-generator/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'landing-page-generator-54133',
+    appId: '1:454396234817:web:c96ae208f99db12d25cac0',
+    storageBucket: 'landing-page-generator-54133.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBKj3Zefgr5fGWSPAGy06sRpprAHqjyB24',
+    authDomain: 'landing-page-generator-54133.firebaseapp.com',
+    messagingSenderId: '454396234817',
+    measurementId: 'G-306XF5KTGQ',
+  },
+};
